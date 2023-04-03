86db64cd6837d185d9e1d8fb4551a329744bb0ed
cvminhquan <minhq460@gmail.com>
2023-04-03 23:08:19 +0700

    +add: foldeer template

---
+++ .fttemplates/TS Function Component/[FTName].tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './[FTName | pascalcase].module.scss';
+
+export interface <FTName | pascalcase>Props {}
+
+export interface <FTName | pascalcase>DataType {}
+
+const <FTName | pascalcase>: React.FC<<FTName | pascalcase>Props> = (props) => {
+  return (
+    <div className={styles['root']}><FTName | pascalcase> component</div>
+  );
+}
+
+export default <FTName | pascalcase>;
