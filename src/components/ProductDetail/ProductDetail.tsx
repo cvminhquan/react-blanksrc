d2bcaa73c755669677e4c4e33df36361d6eabdd5
Minh Quan <chauquan@mobcec.com>
2023-04-26 17:00:25 +0700

    +add: fix small

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './ProductDetail.module.scss';
+import p5 from './../../assets/img/p5.jpg'
 
 const ProductDetail: React.FC = (props) => {
   return (
     <div className='container'>
-      gfasjdhfadjshj
+      <div className="row">
+        <div className="col-lg-3">
+          <div className={styles.hotDealWrapper}>
+            <h3 className={styles.hotDealTitle}>Hot Deal</h3>
+            <div className={styles.productHotDeal}>
+              <div className={styles.productImage}>
+                <img src={p5} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-9">âsds</div>
+      </div>
     </div>
   );
 }
